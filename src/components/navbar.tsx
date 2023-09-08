b29f89574ee03a0ddfc965bc21742c7b52d313ce
Devansh-365 <devanshtiwari365@gmail.com>
2023-09-08 15:08:51 +0530

    feat: update site-nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,8 +2,12 @@ import Link from "next/link";
 
 import { buttonVariants } from "./ui/button";
 import { Icons } from "./icons";
+import { getCurrentUser } from "@/lib/session";
+import React from "react";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const user = await getCurrentUser();
+
   return (
     <header className="fixed top-0 left-0 z-10 w-full border-b border-[rgba(255, 255, 255, 0.08)] backdrop-blur-[12px]">
       <div className="max-w-[120rem] px-8 text-center h-12 flex">
@@ -11,25 +15,39 @@ export default function Navbar() {
           <Icons.logo className="mr-2 h-5 w-5" /> BioBeacon
         </Link>
         <div className="ml-auto flex h-full items-center">
-          <Link
-            className={buttonVariants({
-              size: "sm",
-              className: "rounded-full mr-4",
-              variant: "ghost",
-            })}
-            href="/login"
-          >
-            Log in
-          </Link>
-          <Link
-            className={buttonVariants({
-              size: "sm",
-              className: "rounded-full",
-            })}
-            href="/register"
-          >
-            Sign up
-          </Link>
+          {!user ? (
+            <React.Fragment>
+              <Link
+                className={buttonVariants({
+                  size: "sm",
+                  className: "rounded-full mr-4",
+                  variant: "ghost",
+                })}
+                href="/login"
+              >
+                Log in
+              </Link>
+              <Link
+                className={buttonVariants({
+                  size: "sm",
+                  className: "rounded-full",
+                })}
+                href="/register"
+              >
+                Sign up
+              </Link>
+            </React.Fragment>
+          ) : (
+            <Link
+              className={buttonVariants({
+                size: "sm",
+                className: "rounded-full",
+              })}
+              href="/dashboard"
+            >
+              Dashboard
+            </Link>
+          )}
         </div>
       </div>
     </header>
