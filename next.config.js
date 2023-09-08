f6fc6891fe5ccb6624313afe56444a4b14777e54
Devansh-365 <devanshtiwari365@gmail.com>
2023-09-08 19:58:31 +0530

    fix: display user image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+    ],
   },
 };
 
--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -11,18 +11,14 @@ interface UserAvatarProps extends AvatarProps {
 export function UserAvatar({ user, ...props }: UserAvatarProps) {
   return (
     <Avatar {...props}>
-      <AvatarFallback>
-        <span className="sr-only">{user.name}</span>
-        <Icons.user className="h-4 w-4" />
-      </AvatarFallback>
-      {/* {user.image ? (
+      {user.image ? (
         <AvatarImage alt="Picture" src={user.image} />
       ) : (
         <AvatarFallback>
           <span className="sr-only">{user.name}</span>
           <Icons.user className="h-4 w-4" />
         </AvatarFallback>
-      )} */}
+      )}
     </Avatar>
   );
 }
