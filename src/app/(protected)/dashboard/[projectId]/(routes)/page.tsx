1bf6e9950575cef2bbae8283454b63a0c203c965
Devansh-365 <devanshtiwari365@gmail.com>
2023-09-09 00:41:22 +0530

    fix: mobile mockuo

--- src/app/(protected)/dashboard/[projectId]/(routes)/page.tsx
+++ src/app/(protected)/dashboard/[projectId]/(routes)/page.tsx
@@ -13,7 +13,7 @@ export default function ProjectIdPage({
       <div className="h-full w-screen lg:w-auto lg:basis-3/5 pl-4 pr-4 border-r border-r-gray-200 overflow-scroll">
         <Editor projectId={params?.projectId} />
       </div>
-      <div className="hidden md:my-auto md:flex-1 lg:basis-2/5 pl-4">
+      <div className="hidden w-full h-full max-h-[500px] mt-6 lg:flex lg:basis-3/5 lg:flex-1 pl-4">
         <Mobile projectId={params?.projectId} />
       </div>
       <div className="lg:hidden">
