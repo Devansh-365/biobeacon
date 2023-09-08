1ddef29ad08f312f72f19110a68e8871a100dae1
Devansh-365 <devanshtiwari365@gmail.com>
2023-09-08 23:39:37 +0530

    feat: add toast provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextAuthProvider>
+          <Toaster />
           <ModalProvider />
           {children}
         </NextAuthProvider>
